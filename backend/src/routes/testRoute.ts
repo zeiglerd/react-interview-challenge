d1baf210168e86e9e33b69757d0f90472430dcd7
Michael Hartung <michael.hartung@advisorsexcel.com>
2023-11-20 20:26:05 -0500

    fixed docker issues

--- backend/src/routes/testRoute.ts
+++ backend/src/routes/testRoute.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 export const testRoutes = Router();
 
-// testRoutes.get('/', (_, response) => {
-//   response.send('Hello World');
-// });
+testRoutes.get('/', (_, response) => {
+  response.send('Hello World');
+});

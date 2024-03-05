4838e7e68eb5175b4b4a0dee3926e48df82b0398
Michael Hartung <michael.hartung@advisorsexcel.com>
2024-03-05 11:27:41 -0500

    added credit limit to the accounts table

--- init-db.sql
+++ init-db.sql
@@ -4,7 +4,8 @@ CREATE TABLE accounts (
     account_number INTEGER PRIMARY KEY,
     name VARCHAR NOT NULL,
     amount INTEGER NOT NULL,
-    type VARCHAR NOT NULL
+    type VARCHAR NOT NULL,
+    credit_limit INTEGER
 );
 
 ALTER TABLE accounts ADD CONSTRAINT verify_type
@@ -12,14 +13,14 @@ CHECK (type IN ('checking', 'savings', 'credit'));
 
 -- LOAD DATAS
 INSERT INTO accounts 
-    (account_number, name, amount, type)
+    (account_number, name, amount, type, credit_limit)
 VALUES
     (1, 'Johns Checking', 1000, 'checking'),
     (2, 'Janes Savings', 2000, 'savings'),
-    (3, 'Jills Credit', -3000, 'credit'),
+    (3, 'Jills Credit', -3000, 'credit', 10000),
     (4, 'Bobs Checking', 40000, 'checking'),
     (5, 'Bills Savings', 50000, 'savings'),
-    (6, 'Bills Credit', -60000, 'credit'),
+    (6, 'Bills Credit', -60000, 'credit', 100000),
     (7, 'Nancy Checking', 70000, 'checking'),
     (8, 'Nancy Savings', 80000, 'savings'),
-    (9, 'Nancy Credit', -90000, 'credit');
+    (9, 'Nancy Credit', -90000, 'credit', 100000);

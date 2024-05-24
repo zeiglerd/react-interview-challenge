776e8ad114bd88c8f0d8c3f31cd128567ccc6a20
Michael Hartung <michael.hartung@advisorsexcel.com>
2024-05-24 10:16:18 -0400

    separated the handler logic out of the route

--- api/routes/accounts.ts
+++ api/routes/accounts.ts
@@ -1,31 +1,24 @@
 import express, { Request, Response } from "express";
 import Joi, { Schema } from "joi";
-import pg from "pg";
-
-import { query } from "../utils/db";
+import { getAccount } from "../handlers/accountHandler";
 
 const router = express.Router();
 
-const getAccount: Schema = Joi.string().required();
+const getAccountSchema: Schema = Joi.string().required();
 
 router.get("/:accountID", async (request: Request, response: Response) => {
-  const {error} = getAccount.validate(request.params.accountID);
+  const {error} = getAccountSchema.validate(request.params.accountID);
   
   if (error) {
     return response.status(400).send(error.details[0].message);
   }
-  const res = await query(`
-    SELECT account_number, name, amount, type, credit_limit 
-    FROM accounts 
-    WHERE account_number = $1`,
-    [request.params.accountID]
-  );
 
-  if (res.rowCount === 0) {
-    return response.status(404).send({"error": "Account not found"});
+  try {
+    const account = await getAccount(request.params.accountID);
+    response.send(account);
+  } catch (err) {
+    response.status(404).send({"error": "Account not found"});
   }
-
-  response.send(res.rows[0]);
 });
 
 export default router;
